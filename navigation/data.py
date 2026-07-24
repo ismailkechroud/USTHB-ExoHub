0b744308057cba6c77d36ee8422f1bfeda474023
ismailkechroud <kechroud2020@gmail.com>
2026-07-24 19:17:12 +0100

    Add some modules in L2-ACAD

--- navigation/data.py
+++ navigation/data.py
@@ -1,12 +1,9 @@
-
-
-
 main_navigate = {
     
     "L2": {
         "ACAD": [
-            "Algo 3","Archi 1","Logic", "MI","SI","Proba","Anglais 1", # S3
-            "Archi 2","SE 1","POO","THL","BDD","Anglais 2","Python"    # S4
+            "Algo 3","C","Archi 1","Logic", "MI","SI","Proba","Anglais 1", # S3
+            "Archi 2","SE 1","Linux","THL","POO","Java","BDD","SQL","Anglais 2","Python"    # S4
         ]
     },
 
